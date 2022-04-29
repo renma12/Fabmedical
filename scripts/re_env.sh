6b65cf316cdef14e0dd4e21b5e3d0cbc9dbf8f27
renma12 <renan.p.marasigan@gmail.com>
2022-04-29 04:14:40 +0000

    Initial Commit

---
+++ scripts/re_env.sh
@@ -0,0 +1,8 @@
+export MCW_SUFFIX=599069
+export MCW_GITHUB_USERNAME=renma12
+export MCW_GITHUB_TOKEN=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
+export MCW_GITHUB_URL=https://github.com/renma12/Fabmedical.git
+export MCW_GITHUB_EMAIL=renan.p.marasigan@gmail.com
+export re=gewgom-toqQyg-hozne8
+
+
